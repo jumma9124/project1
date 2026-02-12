94a168cf260ff514ec88b5873a37fdb0ab45cebb
jumma9124 <jumma9124@gmail.com>
2026-02-12 10:42:38 +0900

    fix: BrowserRouter → HashRouter 변경 (GitHub Pages 새로고침 404 해결)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/main/MainPage';
 import LearnPage from './pages/learn/LearnPage';
 import LearnDetailPage from './pages/learn/LearnDetailPage';
@@ -7,14 +7,14 @@ import PortfolioPage from './pages/portfolio/PortfolioPage';
 
 function App() {
   return (
-    <BrowserRouter basename="/project1">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/learn" element={<LearnPage />} />
             <Route path="/learn/:id" element={<LearnDetailPage />} />
         <Route path="/portfolio" element={<PortfolioPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
